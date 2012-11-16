0e253ec08c94c124148095ebdcd163eb4ca4551d
michael <mderazon@hotmail.com>
2012-11-16 18:33:45 +0200

    adding initial page with node.js and twitter bootstrap - rtl

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express')
+  , partials = require('express-partials')
+  , app = express();
+  
+
+exports.init = function(port) {
+
+    app.configure(function(){
+    	app.set('views', __dirname + '/views');
+    	app.set('view engine', 'ejs');
+    	app.use(express.bodyParser());
+		app.use(partials());
+    	app.use(express.methodOverride());
+    	app.use(express.static(__dirname + '/static'));
+    	app.use(app.router);
+    	app.enable("jsonp callback");
+    });
+
+    app.configure('development', function(){
+	   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+        // app.use(express.logger({ format: ':method :url' }));
+    });
+
+    app.configure('production', function(){
+	   app.use(express.errorHandler()); 
+    });
+
+
+    app.use(function(err, req, res, next){
+	   res.render('500.ejs', { locals: { error: err },status: 500 });	
+    });
+
+    app.listen(port);
+
+    console.log("Listening on port %d in %s mode", port, app.settings.env);
+
+    return app;
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+var port = process.env.PORT || 8888;
+var app = require('./app').init(port);
+
+var locals = {
+        title: 		 'CSNC',
+        description: 'Node Express HTML5 & CSS3',
+        author: 	 'Michael D'
+    };
+
+app.get('/', function(req,res){
+    locals.date = new Date().toLocaleDateString();
+    res.render('home.ejs', locals);
+});
+app.get('/about', function(req,res){
+    locals.date = new Date().toLocaleDateString();
+    res.render('about.ejs', locals);
+});
+app.get('/contact', function(req,res){
+    locals.date = new Date().toLocaleDateString();
+    res.render('contact.ejs', locals);
+});
+
+/* The 404 Route (ALWAYS Keep this as the last route) */
+app.get('/*', function(req, res){
+    res.render('404.ejs', {layout:false});
+});
