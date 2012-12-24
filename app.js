e14ecc7fdfd8bb20c37d158239a33152b7aacf6d
michael <mderazon@hotmail.com>
2012-12-24 19:05:03 +0200

    js beautifier

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
-var express = require('express')
-  , partials = require('express-partials')
-  , app = express();
- 
-exports.init = function(port) {
+var express = require('express'),
+    partials = require('express-partials'),
+    app = express();
 
-    app.configure(function(){
-    	app.set('views', __dirname + '/views');
-    	app.set('view engine', 'ejs');
-		app.use(express.bodyParser());
-		app.use(partials());
-    	app.use(express.methodOverride());
-    	app.use(express.static(__dirname + '/static'));
-    	
-		// add middleware to remove trailing slash in urls
-		app.use(function(req, res, next) {
-			if(req.url.substr(-1) == '/' && req.url.length > 1) {
-				res.redirect(301, req.url.slice(0, -1));
-			}
-			next();
-		});
-    	app.use(app.router);
-		app.enable("jsonp callback");
-    });
-	
-		
-    app.configure('development', function(){
-	   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-       app.use(express.logger({ format: ':method :url' }));
+exports.init = function (port) {
+
+    app.configure(function () {
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'ejs');
+        app.use(express.bodyParser());
+        app.use(partials());
+        app.use(express.methodOverride());
+        app.use(express.static(__dirname + '/static'));
+
+        // add middleware to remove trailing slash in urls
+        app.use(function (req, res, next) {
+            if (req.url.substr(-1) == '/' && req.url.length > 1) {
+                res.redirect(301, req.url.slice(0, -1));
+            }
+            next();
+        });
+        app.use(app.router);
+        app.enable("jsonp callback");
     });
 
-    app.configure('production', function(){
-	   app.use(express.errorHandler()); 
+
+    app.configure('development', function () {
+        app.use(express.errorHandler({
+            dumpExceptions: true,
+            showStack: true
+        }));
+        app.use(express.logger({
+            format: ':method :url'
+        }));
     });
 
-    app.use(function(err, req, res, next){
-	   res.render('500.ejs', { locals: { error: err },status: 500 });	
+    app.configure('production', function () {
+        app.use(express.errorHandler());
+    });
+
+    app.use(function (err, req, res, next) {
+        res.render('500.ejs', {
+            locals: {
+                error: err
+            },
+            status: 500
+        });
     });
 
     app.listen(port);
