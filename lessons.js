3b0a054045f8d6e26efec4cba4cc3daba987284e
michael <mderazon@hotmail.com>
2013-01-05 23:55:33 +0200

    refactoring

--- lessons.js
+++ lessons.js
@@ -12,12 +12,12 @@ var lessons = require('./views/lessons/lessons.json').lessons;
 exports.get_lesson_handler = function (req, res, next)
 {
     var lesson = req.params.lesson;
-    res.render('lessons/' + lesson + '.ejs', locals);
+    res.render('lessons/' + lesson + '/' + lesson + '.ejs', locals);
 }
 
 exports.get_all_lessons_handler = function (req, res, next)
 {
 	locals.layout = 'layout';
 	locals.lessons = lessons;
-	res.render('lessons.ejs', locals);
+	res.render('lessons/lessons.ejs', locals);
 }
