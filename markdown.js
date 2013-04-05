4ded4a43cf1b3ee47f2cbf7ae1d48706909d1b73
unknown <mderazon@hotmail.com>
2013-04-06 00:11:33 +0300

    design twekas

--- markdown.js
+++ markdown.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 module.exports = function(file, callback) {
   fs.readFile(file, "utf8", function (err, data) {
     if (err) {
-        return callback(err);
+      return callback(err);
     }
     return callback(marked(data));
   });
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var markdown = require('./markdown');
 var lessons = require('./lessons.json');
 
 app.get('/', function (req, res) {
-    res.locals.controller = 'home';
-    res.render('home');
+  res.locals.controller = 'home';
+  res.render('home');
 });
 
 app.get('/:controller/', function (req, res, next) {
@@ -14,7 +14,7 @@ app.get('/:controller/', function (req, res, next) {
     res.locals.controller = controller;
     res.render(controller);
   } else {
-      next();
+    next();
   }
 });
 
@@ -25,26 +25,26 @@ app.get('/lessons/', function(req, res) {
 });
 
 app.get('/lessons/:name/', function(req, res, next) {
-    res.locals.controller = 'lessons';
-    var name = req.params.name;
-    var lesson = lessons[name];
-    if (!lesson) {
-      return next();
+  res.locals.controller = 'lessons';
+  var name = req.params.name;
+  var lesson = lessons[name];
+  if (!lesson) {
+    return next();
+  }
+  lesson.path = 'lessons/' + name;
+  lesson.file = lesson.path + '/' + name + '.md';
+  markdown(lesson.file, function(result, err) {
+    if (err) {
+      console.log(err);
     }
-    lesson.path = 'lessons/' + name;
-    lesson.file = lesson.path + '/' + name + '.md';
-    markdown(lesson.file, function(result, err) {
-      if (err) {
-        console.log(err);
-      }
-      lesson.body = result;
-      res.locals.lesson = lesson;
-      res.render('lesson');
-    });
+    lesson.body = result;
+    res.locals.lesson = lesson;
+    res.render('lesson');
+  });
 });
 
 /* The 404 Route (ALWAYS Keep this as the last route) */
 app.get('/*', function (req, res) {
-    console.log('got 404 request to ' + req.url);
-    res.render('404');
+  console.log('got 404 request to ' + req.url);
+  res.render('404');
 });
