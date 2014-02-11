a6b9fc4ba12a147acd2227e8c3c9b51f9f099082
michael <mderazon@hotmail.com>
2014-02-11 01:34:20 -0500

    80 percent ready

---
+++ public/_scripts/script.js
@@ -0,0 +1,39 @@
+
+if (window.location.pathname === '/search-results') {
+  var Swiftype = window.Swiftype || {};
+  (function() {
+   Swiftype.key = 'FbKv4mDi54Z5vEeYzz8i';
+   Swiftype.inputElement = '#st-search-input';
+   Swiftype.resultContainingElement = '#st-results-container';
+   Swiftype.attachElement = '#st-search-input';
+   Swiftype.renderStyle = "inline";
+
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.async = true;
+   script.src = "//swiftype.com/embed.js";
+   var entry = document.getElementsByTagName('script')[0];
+   entry.parentNode.insertBefore(script, entry);
+}());
+} else {
+  var Swiftype = window.Swiftype || {};
+  (function() {
+    Swiftype.key = 'FbKv4mDi54Z5vEeYzz8i';
+    Swiftype.inputElement = '#st-search-input';
+    Swiftype.resultContainingElement = '#st-results-container';
+    Swiftype.attachElement = '#st-search-input';
+    Swiftype.renderStyle = "new_page";
+
+    Swiftype.resultPageURL = '/search-results';
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = "//swiftype.com/embed.js";
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+  }());
+
+}
+
+
